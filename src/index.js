a5504e62de7d30a539281db2c6d20e93505085f3
Austin Dang <91802108+AuDang@users.noreply.github.com>
2022-03-23 12:38:16 -0700

    JSX Contact info

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import ContactUs from './ContactUs'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
